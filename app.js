48c4ee86cb368848ed0f05a92d9d1e2e29969c37
Santi <camelo.santiago9@gmail.com>
2025-03-01 10:13:04 -0600

    app.js

--- app.js
+++ app.js
@@ -14,25 +14,38 @@ function agregarAmigo(){
     console.log(amigos);
     //Limpiar caja
     document.getElementById('amigo').value = '';
-    console.log(amigos.length);
     //Actualizar la lista amigos en pantalla
     listaAmigos();
+}
 
-    //funcion para obtener elemento de la lista
-    function listaAmigos(){
-        let listaElement = document.getElementById("listaAmigos");
-        //limpiar lista existente
-        listaElement.innerHTML = "";
-        //repetir lista
-        for(let i = 0; i < amigos.length; i++){
-            console.log('listaAmigos');       
-                }
+//funcion para obtener elemento de la lista
+function listaAmigos(){
+    let listaElement = document.getElementById("listaAmigos");
+    //limpiar lista existente
+    listaElement.innerHTML = "";
+    //repetir lista
+    for(let i = 0; i < amigos.length; i++){
+        console.log('listaAmigos');
         //creamos un nuevo elemento para cada amigo
         let nuevoNombre = document.createElement('li');
-        nuevoNombre.textContent = amigos;
+        nuevoNombre.textContent = amigos[i];
         //agregamos los nuevos elementos a la lista
-        listaElement.appendChild(nuevoNombre);
-        console.log(nuevoNombre);
-    } 
+            listaElement.appendChild(nuevoNombre);
+            console.log(nuevoNombre);
+    }
 }
-console.log
+function sortearAmigo(){
+    //validacion que haya amigos en la lista
+    if (amigos.length === 0){
+        alert ("No hay amigos para sortear");
+        return;
+    }
+    //generar nombre aleatorio de la lista de amigos
+    let indiceAleatorio = Math.floor(Math.random()* amigos.length);
+    console.log(indiceAleatorio);
+    //obtener el nombre sorteado
+    let amigoSorteado = amigos[indiceAleatorio];
+    //mostrar resultado
+    let resultadoElement = document.getElementById('resultado');
+    resultadoElement.innerHTML = `el amigo sorteado es:${amigoSorteado}`;
+}
